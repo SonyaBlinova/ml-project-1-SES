a98259cd9d0708f6612643a78d318a1a18acdff0
“charkaoui <saadcherkaoui09@gmail.com>
2021-10-23 23:01:31 +0200

    bias/variance decomp for RR + cross for RR

--- plots.py
+++ plots.py
@@ -14,6 +14,18 @@ def cross_validation_visualization(lambds, mse_tr, mse_te, bd_left, bd_right):
     plt.grid(True)
     plt.savefig("cross_validation")
     
+def cross_validation_viz(lambds, mse_tr, mse_te):
+    """visualization the curves of mse_tr and mse_te."""
+    plt.semilogx(lambds, mse_tr, marker=".", color='b', label='train error')
+    plt.semilogx(lambds, mse_te, marker=".", color='r', label='test error')
+    plt.xlabel("lambda")
+    plt.ylabel("rmse")
+    plt.xlim(1e-4, 1)
+    plt.title("cross validation")
+    plt.legend(loc=2)
+    plt.grid(True)
+    plt.savefig("cross_validation")
+    
 def bias_variance_decomposition_visualization(degrees, rmse_tr, rmse_te):
     """visualize the bias variance decomposition."""
     rmse_tr_mean = np.expand_dims(np.mean(rmse_tr, axis=0), axis=0)
@@ -44,10 +56,9 @@ def bias_variance_decomposition_visualization(degrees, rmse_tr, rmse_te):
         linestyle="-",
         label='test',
         linewidth=3)
-    plt.xlim(1, 9)
-    plt.ylim(0.2, 0.7)
-    plt.xlabel("degree")
+    plt.xlabel("lambdas")
     plt.ylabel("error")
     plt.legend(loc=1)
     plt.title("Bias-Variance Decomposition")
     plt.savefig("bias_variance")
+
