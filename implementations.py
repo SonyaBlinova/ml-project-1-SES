01df310ee4a22810253e3da6a4bac03c3e5eed72
Ekaterina <trimbachkatya@gmail.com>
2021-10-25 00:50:15 +0200

    rewrote SGD

--- implementations.py
+++ implementations.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 from utils import *
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma, plot_loss = False):
@@ -73,9 +74,8 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma, plot_loss = False):
     """
 
     def df(y, tx, w):
-        batch_size = 1
-        y_batch, tx_batch = next(batch_iter(y, tx, batch_size=batch_size, num_batches=1, shuffle=True))
-        return (-1/y_batch.shape[0])*np.dot(tx_batch.T, y_batch - np.dot(tx_batch, w))
+        idx = random.choice(range(w.shape[0]))
+        return (-1/y.shape[0])*np.dot(tx.T[idx], y - np.dot(tx, w))
         
     w, steps = gradient_descent(df, y, tx, initial_w, gamma, max_iters, return_all_steps = True)
     
