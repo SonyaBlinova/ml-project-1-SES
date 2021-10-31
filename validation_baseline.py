c9c6512afb397bfa81863fd3c1a2277b1db18f36
SonyaBlinova <sofa_blinova@mail.ru>
2021-10-31 02:34:33 +0200

    cross validation befor preproccessing

--- validation_baseline.py
+++ validation_baseline.py
@@ -6,7 +6,7 @@ from utils import *
 from plots import *
 from proj1_helpers import *
 
-def cross_validation(type_, y, x, k_indices, k, lambda_ = None, gamma = None, initial_w = None, max_iters = None, degree=0):
+def cross_validation_baseline(type_, y, x, k_indices, k, lambda_ = None, gamma = None, initial_w = None, max_iters = None, degree=0):
     """return the loss of ridge regression."""
     # get k'th subgroup in test, others in train
     not_k_indices = set(np.arange(len(x))) - set(k_indices[k])
@@ -36,24 +36,24 @@ def cross_validation(type_, y, x, k_indices, k, lambda_ = None, gamma = None, in
         y_pred = predict_labels(w, x_test_poly)
         accuracy_  =  accuracy(y_pred, y_test)  
     elif type_ == 'RR':
-        w, loss = ridge_regression(y_trains[i], x_train_poly, lambda_)
+        w, loss = ridge_regression(y_train, x_train_poly, lambda_)
         y_pred = predict_labels(w, x_test_poly)
         accuracy_  =  accuracy(y_pred, y_test)  
     elif type_ == 'LR':
         initial_w = np.random.rand((x_train_poly.shape[1]))
-        w, loss = logistic_regression(y_trains[i], x_train_poly, initial_w, max_iters, gamma, plot_loss = False)
+        w, loss = logistic_regression(y_train, x_train_poly, initial_w, max_iters, gamma, plot_loss = False)
         y_pred = predict_labels_for_log(w, x_test_poly)
         accuracy_  =  accuracy(y_pred, y_test)  
     elif type_ == 'RLR':
         initial_w = np.random.rand((x_train_poly.shape[1]))
-        w, loss = reg_logistic_regression(y_trains[i], x_train_poly, lambda_, initial_w, max_iters, gamma, plot_loss = False)
+        w, loss = reg_logistic_regression(y_train, x_train_poly, lambda_, initial_w, max_iters, gamma, plot_loss = False)
         y_pred = predict_labels_for_log(w, x_test_poly)
         accuracy_  =  accuracy(y_pred, y_test)  
     else:
         raise TypeError(f"{type_} Wrong type!")
     return accuracy_
 
-def cross_validation_demo(type_, y, tx, bd_left, bd_right, seed, gammas=None, max_iters=None, lambdas=None, degrees=None):
+def cross_validation_demo_baseline(type_, y, tx, bd_left, bd_right, seed, gammas=None, max_iters=None, lambdas=None, degrees=None):
     k_fold = 3
     # split data in k fold
     k_indices = build_k_indices(y, k_fold, seed)
@@ -85,11 +85,11 @@ def cross_validation_demo(type_, y, tx, bd_left, bd_right, seed, gammas=None, ma
             acc_batch = []
             for k in range(k_fold):
                 if type_ in ['GD', 'SGD', 'LR']:
-                    acc_ = cross_validation(type_=type_, y=y, x=tx, k_indices=k_indices, k=k, gamma = param_2, max_iters = param_1)
+                    acc_ = cross_validation_baseline(type_=type_, y=y, x=tx, k_indices=k_indices, k=k, gamma = param_2, max_iters = param_1)
                 elif type_ in ['RR']:
-                    acc_ = cross_validation(type_=type_, y=y, x=tx, k_indices=k_indices, k=k, lambda_=param_2, degree=param_1)
+                    acc_ = cross_validation_baseline(type_=type_, y=y, x=tx, k_indices=k_indices, k=k, lambda_=param_2, degree=param_1)
                 elif type_ in ['RLR']:
-                    acc_ = cross_validation(type_=type_, y=y, x=tx, k_indices=k_indices, k=k, gamma = gammas, max_iters = max_iters, lambda_=param_2)
+                    acc_ = cross_validation_baseline(type_=type_, y=y, x=tx, k_indices=k_indices, k=k, gamma = gammas, max_iters = max_iters, lambda_=param_2)
                 else:
                     raise NotImplementedError
                 acc_batch.append(acc_)
@@ -109,8 +109,10 @@ def cross_validation_demo(type_, y, tx, bd_left, bd_right, seed, gammas=None, ma
         else:
             plt.semilogx(params_2, global_acc[i], marker=".", label=label)
             plt.xlim(10**bd_left, 10**bd_right)
-    plt.xlabel(xlabel, fontsize=15)
-    plt.ylabel("Accuracy", fontsize=15)
-    plt.title("Cross validation of the " + type_, fontsize=20)
+    plt.xlabel(xlabel, fontsize=17)
+    plt.ylabel("Accuracy", fontsize=17)
+    plt.title("Cross validation for the " + type_, fontsize=20)
     plt.legend(fontsize=15)
     plt.grid(True)
+    ax.tick_params(axis='both', which='major', labelsize=13)
+    plt.savefig('data/cross_val_basline'+type_+'.png')
